ca238ab862205abdcb2e2ae173d2695037639154
muquit <muquit@gmail.com>
2015-10-25 16:45:30 -0400

    bat script to initialize VisualStudio 14

---
+++ init_vs14.bat
@@ -0,0 +1,7 @@
+REM your env could be different
+@echo %VS100COMMONTOOLS%
+REM 
+REM "%VS100COMMONTOOLS%\vsvars32.bat
+REM "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
+REM Oct-25-2015
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
